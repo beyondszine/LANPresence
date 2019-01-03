57cdbaeaa05e8b2668cb2a038363b0681b4ac78a
beyondszine <saurabhshandilya.1991@gmail.com>
2019-01-04 02:07:32 +0530

    pushing ip to mac data to influxdb

---
+++ app.js
@@ -0,0 +1,69 @@
+const Influx = require('influx');
+const http = require('http');
+const os = require('os');
+const arpScanner = require('arpscan');
+
+
+var options={
+    command :   'arp-scan',
+    args    :   ['-l'],
+    interface : 'wlp2s0',
+    sudo    :   true
+};
+
+const influx = new Influx.InfluxDB({
+  host: 'localhost',
+  database: 'Presence',
+  schema: [
+    {
+      measurement: 'presenceIdentifier',
+      fields: {
+        macaddress: Influx.FieldType.STRING,
+        IPaddress: Influx.FieldType.STRING
+      },
+      tags: [
+        'host'
+      ]
+    }
+  ]
+})
+
+function onResult(err, data){
+    if(err){
+        throw err;
+    }
+    else{
+        data.forEach(function(arpresponse){
+            console.log(arpresponse.ip,arpresponse.mac);
+            influx.writePoints([
+                {
+                  measurement: 'presenceIdentifier',
+                  tags: { host: os.hostname() },
+                  fields: { macaddress : arpresponse.mac, IPaddress: arpresponse.ip},
+                }
+              ]).catch(err => {
+                console.error(`Error saving data to InfluxDB! ${err.stack}`);
+              });
+        });
+    } 
+};
+
+
+/**
+ * Now, we'll make sure the database exists and boot the app.
+ */
+influx.getDatabaseNames()
+  .then(names => {
+    if (!names.includes('Presence')) {
+      return influx.createDatabase('Presence')
+    }
+  })
+  .then(() => {
+    // http.createServer(app).listen(4000, function () {
+    //   console.log('Listening on port 4000')
+    // })
+    arpScanner(onResult, options);
+  })
+  .catch(err => {
+    console.error(`Error creating Influx database!`)
+  })
